035cf551c8736a726fb264260642e24d98a7af49
Prithvi Raj Pani <prithvirajpani@PMac.local>
2026-06-10 01:18:47 +0530

    Adding backend API URL

--- js/api.js
+++ js/api.js
@@ -1,3 +1,5 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbytSuDftiIWfMNNbhx4TFeS6nYxVJBA_NjOGMs9rghHVHk4FXVYIRwHc95db80ArkA7hg/exec";
+
 async function api(action, params = {}) {
 
     const qs =
